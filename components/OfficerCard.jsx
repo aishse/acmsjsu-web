106f313ba3627aeb6aac91c2a2a0b5839a40f35f
aishse <email.friendme@gmail.com>
2023-09-07 18:45:06 -0700

    formatting officercards

--- components/OfficerCard.jsx
+++ components/OfficerCard.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import Image from 'next/image'
-import LinkedIn from '@/public/icons/linkedin.png'
+import React from "react";
+import Image from "next/image";
+import LinkedIn from "@/public/icons/linkedin.png";
 
-const OfficerCard = ({ photo, name, role, linkedin}) => {
+const OfficerCard = ({ photo, name, role, linkedin }) => {
   return (
     <div className="transition duration-200 ease-in-out hover:shadow-2xl flex flex-col justify-center items-center border-[3px] border-[#eabc4e] gap-2 py-2 text-[16px] sm:text-sm rounded-xl shadow-lg">
       <h1 className="font-bold">{role}</h1>
-      <Image src={photo} alt="Officer" width={1000} height={1000} className="object-cover w-40 h-40" />
+      <Image
+        src={photo}
+        alt="Officer"
+        width={100}
+        height={100}
+        className="p-0 object-cover w-40 h-40"
+      />
       <h1 className="font-bold">{name}</h1>
       <div>
-        <a><Image src={LinkedIn} width={100} height={100} className="w-5 h-5"/></a>
+        <a>
+          <Image src={LinkedIn} width={100} height={100} className="w-5 h-5" />
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OfficerCard
+export default OfficerCard;
--- components/Officers.jsx
+++ components/Officers.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import OfficerCard from './OfficerCard'
-import Trique from '@/public/photos/officers/trique.jpg'
-import Karthik from '@/public/photos/officers/Karthik.png'
-import Shirley from '@/public/photos/officers/shirleyli.png'
-import Anne from '@/public/photos/officers/annemai.jpg'
-import Bineet from '@/public/photos/officers/bineet.png'
-import Galit from '@/public/photos/officers/galitbolotin.png'
-import Tim from '@/public/photos/officers/tim.png'
-
-
+import React from "react";
+import OfficerCard from "./OfficerCard";
+import Trique from "@/public/photos/officers/trique.jpg";
+import Karthik from "@/public/photos/officers/Karthik.png";
+import Shirley from "@/public/photos/officers/shirleyli.png";
+import Anne from "@/public/photos/officers/annemai.jpg";
+import Bineet from "@/public/photos/officers/bineet.png";
+import Galit from "@/public/photos/officers/galitbolotin.png";
+import Tim from "@/public/photos/officers/tim.png";
 
 const Officers = () => {
   const officers = [
@@ -16,55 +14,62 @@ const Officers = () => {
       name: "Karthik Manishankar",
       role: "President",
       photo: Karthik,
-      linkedin: "https://www.linkedin.com/in/karthikmanishankar/"
+      linkedin: "https://www.linkedin.com/in/karthikmanishankar/",
     },
     {
       name: "Shirley Li",
       role: "Vice President",
       photo: Shirley,
-      linkedin: "https://www.linkedin.com/in/shirley-shuhua-li/"
+      linkedin: "https://www.linkedin.com/in/shirley-shuhua-li/",
     },
     {
       name: "Galit Bolotin",
       role: "Event Chair",
       photo: Galit,
-      linkedin: "https://www.linkedin.com/in/gbolotin/"
+      linkedin: "https://www.linkedin.com/in/gbolotin/",
     },
     {
       name: "Anne Mai",
       role: "Event Chair",
       photo: Anne,
-      linkedin: "https://www.linkedin.com/in/annepmai/"
+      linkedin: "https://www.linkedin.com/in/annepmai/",
     },
     {
       name: "Bineet Anand",
       role: "Social Media",
       photo: Bineet,
-      linkedin: "https://www.linkedin.com/in/bineet-anand/"
+      linkedin: "https://www.linkedin.com/in/bineet-anand/",
     },
     {
       name: "Timnothy Kim",
       role: "Treasurer",
       photo: Tim,
-      linkedin: "https://www.linkedin.com/in/timothy-kim712/"
+      linkedin: "https://www.linkedin.com/in/timothy-kim712/",
     },
     {
       name: "Trique Nguyen",
       role: "Dev Team Lead",
       photo: Trique,
-      linkedin: "https://www.linkedin.com/in/triquenguyen"
+      linkedin: "https://www.linkedin.com/in/triquenguyen",
     },
-  ]
+  ];
   return (
     <div className="flex flex-col items-center justify-center gap-8 max-w-[1080px]">
-      <h1 className="font-black text-4xl text-[#196096] sm:text-3xl">Our Officers</h1>
+      <h1 className="font-black text-4xl text-[#196096] sm:text-3xl">
+        Our Officers
+      </h1>
       <div className="xl:grid xl:grid-cols-4 sm:flex sm:flex-wrap md:flex md:flex-wrap gap-8 items-center justify-center">
         {officers.map((officer) => (
-          <OfficerCard name={officer.name} role={officer.role} photo={officer.photo} linkedin={officer.linkedin} />
+          <OfficerCard
+            name={officer.name}
+            role={officer.role}
+            photo={officer.photo}
+            linkedin={officer.linkedin}
+          />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Officers
+export default Officers;
